9f5a82feac201dd8bfeeeb6b1074447b633f5a52
i069498 <miles.chen01@sap.com>
2015-11-24 18:05:09 +0800

    enable ssl for nodejs-hs

--- jenkins/README.md
+++ jenkins/README.md
@@ -6,5 +6,10 @@
      -v /tmp:/tmp \
      -v ~/.jenkins:/.jenkins \
      -v /var/run/docker.sock:/var/run/docker.sock \
-     -p 8000:8080 jenkins:1.634
+     -p 8000:8080 jenkins:1.638
 
+# Debian repo
+https://apt.dockerproject.org/repo/dists/debian-jessie/main/
+https://apt.dockerproject.org/repo/pool/main/d/docker-engine/
+https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_1.8.3-0~jessie_amd64.deb
+https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_1.9.0-0~jessie_amd64.deb
--- jenkins/build.sh
+++ jenkins/build.sh
@@ -1,10 +1,14 @@
 #!/usr/bin/env bash
 
-JENKINS_VER='1.634'
+JENKINS_VER='1.638'
+DOCKER_VER='1.8.3'
 
 if [ ! -f jenkins.war ]; then
+    curl -LO http://10.58.136.166/amd64-usr/815.0.0/fin.pem
     export https_proxy=http://proxy.wdf.sap.corp:8080
     export http_proxy=http://proxy.wdf.sap.corp:8080
-    curl -LO http://mirrors.jenkins-ci.org/war/1.634/jenkins.war
+    curl -LO http://mirrors.jenkins-ci.org/war/$JENKINS_VER/jenkins.war
+    curl -LO https://apt.dockerproject.org/repo/pool/main/d/docker-engine/docker-engine_$DOCKER_VER-0~jessie_amd64.deb
+    mv "docker-engine_${DOCKER_VER}-0~jessie_amd64.deb" docker-engine_jessie_amd64.deb
 fi
 docker build --no-cache=true -t jenkins:$JENKINS_VER .
